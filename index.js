f79a91ebe4b078c01fbe3fd8b168d8db82bfa49a
unknown <sikderfahad>
2023-05-26 16:59:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// const chefs = require("./data.json");
+
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Chef server is running ...!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
